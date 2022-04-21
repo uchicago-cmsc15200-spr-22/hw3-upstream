405f145bcfba6e4038e337f30d1fb270b65e5758
Hannah Morgan <hannah.mairs@gmail.com>
2022-04-21 08:18:48 -0500

    Homework #3 material

--- grader.py
+++ grader.py
@@ -14,7 +14,7 @@ RUBRIC = [
           ("Exercise 2: Copy color", "copy_color", 4.0),  
           ("Exercise 4: Luminance", "luminance", 4.0),
           ("Exercise 5: Grayscale", "grayscale", 4.0), 
-          ("Exercise 6: Make stripe", "make_strip", 10.0),  
+          ("Exercise 6: Make stripe", "make_stripe", 10.0),  
           ("Exercise 8: Pack color", "pack_color", 4.0),  
           ("Exercise 9: Unpack colors", "unpack_colors", 10.0),  
           ("Exercise 10: Shopping list amounts", "shopping_list_amounts", 10.0),  
